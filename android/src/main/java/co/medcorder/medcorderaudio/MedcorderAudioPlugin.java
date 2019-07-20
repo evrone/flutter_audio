cf2cb69f0b5ee926c5d234581ba6b3670a12120d
fd <shadowcat.liuyang@gmail.com>
2019-07-20 17:01:05 +0800

    更新AndroidX 兼容性问题

--- android/src/main/java/co/medcorder/medcorderaudio/MedcorderAudioPlugin.java
+++ android/src/main/java/co/medcorder/medcorderaudio/MedcorderAudioPlugin.java
@@ -8,7 +8,8 @@ import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.util.Log;
-import android.support.v4.content.ContextCompat;
+
+import androidx.core.content.ContextCompat;
 
 import java.io.File;
 import java.util.HashMap;
